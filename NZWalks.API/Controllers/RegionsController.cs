a18ab8822a919f73e0a7c6c544562b50aba33f78
peterdang <peterdangvnn@gmail.com>
2023-10-03 16:11:04 +0700

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:1234/api
+    [Route("api/regions")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentName = new string[] { "John", "Jane" };
+            return Ok(studentName);
+        }
+    }
+}
